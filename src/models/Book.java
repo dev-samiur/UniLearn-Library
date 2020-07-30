fac1fdb983c67bff1c401204673b4747160e2e4e
samirRahman <samiurakif@gmail.com>
2020-07-31 04:07:05 +0600

    solve merge conflict with data_logic branch

--- src/models/Book.java
+++ src/models/Book.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.Statement;
-//import java.sql.PreparedStatement;
+import java.sql.PreparedStatement;
 
 public class Book {
 	

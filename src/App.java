f2bd0b4cfc5907cc7b3f84070d3e3919da713a31
samirRahman <samiurakif@gmail.com>
2020-07-31 13:23:14 +0600

    App file edited

--- src/App.java
+++ src/App.java
@@ -1,28 +1,8 @@
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import models.Book;
-
 public class App {
 	
 	public static void main(String[] args) {
 		
-		
-		try {
-			
-			Book obj= new Book();
-			ResultSet rs= obj.findAll();
-			
-			while( rs.next())
-			{
-				System.out.println(rs.getInt("book_fees"));
-			}
-	
-			
-	    }catch (SQLException e) {
-	            System.out.println(e.getMessage());
-	    }
 	}
 
 }
